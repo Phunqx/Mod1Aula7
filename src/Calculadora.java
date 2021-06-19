4df8c0a44ed580b6041a79c7e93ea8329f941a2d
John Leite <joatech@hotmail.com>
2021-06-19 13:09:16 -0300

    Adição de funções na calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,15 +1,52 @@
 import java.util.Scanner;
 
 public class Calculadora {
-
+/* Criar as funções para soma, subtração, divisão e multiplicação.
+ * Criar também uma função para exibir o resultado.
+ * A digitação dos valores deve ser mantida no main.
+ */
+	
+	public static void exibirResultado(double valor) {
+		System.out.println("O resultado foi " + valor + "!");
+	}
+	
+	public static void soma(double valor1, double valor2) {
+		System.out.println("Realizando a soma entre " + valor1 + " e " + valor2);
+		double soma = valor1 + valor2;
+		exibirResultado(soma);
+	}
+	
+	public static void subtracao(double valor1, double valor2) {
+		System.out.println("Realizando a subtração entre " + valor1 + " e " + valor2);
+		double soma = valor1 - valor2;
+		exibirResultado(soma);
+	}
+	
+	public static void multiplicacao(double valor1, double valor2) {
+		System.out.println("Realizando a multiplicação entre " + valor1 + " e " + valor2);
+		double soma = valor1 * valor2;
+		exibirResultado(soma);
+	}
+	
+	public static void divisao(double valor1, double valor2) {
+		System.out.println("Realizando a divisão entre " + valor1 + " e " + valor2);
+		double soma = valor1 / valor2;
+		exibirResultado(soma);
+	}
+	
+	public static double pegarInput(Scanner leitor, int index) {
+		System.out.print("Digite o " + index + "º valor:");
+		double valor = leitor.nextDouble();
+		return valor;
+	}
+	
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
 		double valor1 = 0, valor2=0;
-		double soma, subtracao, divisao, multiplicacao;
 		int opcao = 0;
 		
 		while (opcao!=6) {
-			System.out.println("PROGRAMA CALCULADORA");
+			System.out.println("\n\nPROGRAMA CALCULADORA");
 			System.out.println("Escolha sua opção!");
 			System.out.println("1 - Digitar valores");
 			System.out.println("2 - Realizar soma");
@@ -17,38 +54,26 @@ public class Calculadora {
 			System.out.println("4 - Realizar divisão");
 			System.out.println("5 - Realizar multiplicação");
 			System.out.println("6 - Sair");
+			System.out.print("Opt: ");
 			opcao = leitor.nextInt();
 			
 			switch(opcao) {
 			case 1:
-				System.out.println("\n\nDigite o 1º valor:");
-				valor1 = leitor.nextDouble();
-				System.out.println("\n\nDigite o 2º valor:");
-				valor2 = leitor.nextDouble();
-				
-				System.out.println("Os valores " + valor1 + " e " + valor2 + " foram armazenados\n\n");
-				
+				valor1 = pegarInput(leitor,1);
+				valor2 = pegarInput(leitor,2);
+				System.out.println("Os valores " + valor1 + " e " + valor2 + " foram armazenados");          
 				break;
 			case 2:
-				System.out.println("\n\nRealizando a soma entre " + valor1 + " e " + valor2);
-				soma = valor1 + valor2;
-				System.out.println("O resultado foi " + soma + "!\n\n");
-				
+				soma(valor1,valor2);
 				break;
 			case 3:
-				System.out.println("\n\nRealizando a subtração entre " + valor1 + " e " + valor2);
-				subtracao = valor1 - valor2;
-				System.out.println("O resultado foi " + subtracao + "!\n\n");
+				subtracao(valor1,valor2);
 				break;
 			case 4:
-				System.out.println("\n\nRealizando a divisão entre " + valor1 + " e " + valor2);
-				divisao = valor1 / valor2;
-				System.out.println("O resultado foi " + divisao + "!\n\n");
+				divisao(valor1,valor2);
 				break;
 			case 5:
-				System.out.println("\n\nRealizando a multiplicação entre " + valor1 + " e " + valor2);
-				multiplicacao = valor1 * valor2;
-				System.out.println("O resultado foi " + multiplicacao + "!\n\n");
+				multiplicacao(valor1,valor2);
 				break;
 			case 6:
 				System.out.println("Saindo do sistema");
@@ -57,8 +82,6 @@ public class Calculadora {
 			
 		}
 		leitor.close();
-		
-
 	}
 
 }
